53450520b4d59e07cb0676deebea68797b0c562f
codersjj <shanewestlife@outlook.com>
2024-04-16 22:26:33 +0800

    feat(Testing Your First Application - Installing Cypress and writing your first test): Aside: Getting elements best practices

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,8 +1,7 @@
 describe('home page', () => {
   it('the h1 contains the correct text', () => {
     cy.visit('http://localhost:3000')
-    cy.get('h1')
-      .should('exist')
+    cy.get('[data-test="hero-heading"]')
       .contains('Testing Next.js Applications with Cypress')
   })
 })
