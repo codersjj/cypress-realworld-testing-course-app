47fac2faedb891bdce2375e27f283ec5f7fa3504
codersjj <shanewestlife@outlook.com>
2024-04-16 23:31:20 +0800

    feat(Testing Your First Application - Installing Cypress and writing your first test): Using only to run a single test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -5,7 +5,9 @@ describe('home page', () => {
       .contains('Testing Next.js Applications with Cypress')
   })
 
-  it('the features on the homepage are correct', () => {
+  it.only('the features on the homepage are correct', () => {
     cy.visit('http://localhost:3000')
+    cy.get('dt').eq(0).contains('4 Courses')
+    // cy.get('dt').eq(0).contains(/4 courses/i)
   })
 })
