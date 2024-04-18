36fbc6a0ad9c9ae4068cbd26bd6ef4b7b27960fb
codersjj <shanewestlife@outlook.com>
2024-04-18 23:03:17 +0800

    feat(Testing Your First Application - How to test user journeys): Practice

--- cypress/e2e/user-journey.cy.ts
+++ cypress/e2e/user-journey.cy.ts
@@ -17,5 +17,16 @@ describe('User journey', () => {
       'eq',
       '/testing-your-first-application/installing-cypress-and-writing-our-first-test'
     )
+
+    cy.getByData('challenge-answer-0').click()
+    cy.getByData('next-lesson-button').should('exist').click()
+    cy.location('pathname').should(
+      'eq',
+      '/testing-your-first-application/setting-up-data-before-each-test'
+    )
+
+    cy.getByData('challenge-answer-0').click()
+    cy.getByData('next-lesson-button').should('exist').contains('Complete Course').click()
+    cy.location('pathname').should('eq', '/')
   })
 })
